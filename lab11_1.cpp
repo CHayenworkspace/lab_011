e8d18dc35a1cebcecb2ee66467004ce86f6e7e0c
CHayen030 <chayenlol268@gmail.com>
2024-12-20 16:31:38 +0700

    complete

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,17 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<string>
+
+using namespace std;
+int main(){
+    srand(time(0));
+    string x ;
+    string G[9] = {"A","B+","B","C+","C","D+","D","F","W"};
+    cout << "Press Enter 3 times to reveal your future.\n";    
+    getline(cin,x);
+    if(x==""){getline(cin,x);
+    if(x==""){getline(cin,x);
+    if(x==""){
+    cout << "You will get "<< G[rand()%9+1]<< " in this 261102.";}}}}
+
